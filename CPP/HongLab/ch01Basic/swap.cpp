144a2256762b614ca25811975169a4a6ed6d3212
Jake1152 <rancho974@gmail.com>
2024-02-12 23:03:23 +0900

    [FIX] using function pointer in test function

--- CPP/HongLab/ch01Basic/swap.cpp
+++ CPP/HongLab/ch01Basic/swap.cpp
@@ -8,14 +8,54 @@ void swap(int &a, int &b) {
   b = temp;
 }
 
+/*
+// lecture cases
+int MySwapValue(int i, int j)
+{
+  // TOOD
+  // return ?;
+  return 0;
+}
+*/
+
+void MySwapPtr(int* i, int* j)
+{
+  int *temp;
+
+  temp = i;
+  i = j;
+  j = temp;
+}
+
+void MySwapRef(int& i, int& j)
+{
+  int temp;
+
+  temp = i;
+  i = j;
+  j = temp;
+}
+
+template <typename T>
+void doSwapTest(int& a, int& b, void (*swapFn)(T&, T&))
+{
+  std::cout << a << " " << b << std::endl;
+  swapFn(a, b);
+  std::cout << a << " " << b << std::endl;
+}
+
 int main() {
   int a = 3;
   int b = 2;
+  //void (*fnPtr)(int&, int&) = MySwapRef;
 
+  doSwapTest(a, b, MySwapRef);
+  //fnPtr(a, b);
+  /*
   std::cout << a << " " << b << std::endl;
-  swap(a, b);
+  swapFn(a, b, fnPtr(a, b));
   std::cout << a << " " << b << std::endl;
-
+  */
   return 0;
 }
 
