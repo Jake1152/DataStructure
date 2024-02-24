4ab28e8b18a56a30670b95ede0cb7c7911012fff
Jake1152 <rancho974@gmail.com>
2024-02-25 02:10:24 +0900

    [ADD] 배열의 정렬 상태 확인 하는 로직
    - 하드코딩 된 두개 값이 오름차순으로 정렬되어있는지 확인
    - 첫번째 인자를 배열로 변경한 버젼

--- CPP/HongLab/ch01Basic/swap.cpp
+++ CPP/HongLab/ch01Basic/swap.cpp
@@ -64,6 +64,14 @@ void doSwapTest(T a, T b, void (*swapFn)(T, T))
   std::cout << a << " " << b << std::endl;
 }
 
+void compareTwoValue(int a, int b)
+{
+  std::string compareResult = "false";
+  if (b >= a)
+    compareResult = "true";
+  std::cout << a << " " << b << " " << compareResult << std::endl;
+}
+
 int main() {
   int a = 3;
   int b = 2;
@@ -78,8 +86,8 @@ int main() {
   /** Sort
    * 배열 원소 2개만 있을 때의 정렬
   */
-  std::cout << "### Sort ###"<< std::endl;
   {
+    std::cout << "### Sort ###"<< std::endl;
     int arr[] = { 3, 2 };
     std::cout << "Before sort : " << arr[0] << " " << arr[1] << std::endl;
 
@@ -88,6 +96,37 @@ int main() {
       swap(arr[0], arr[1]);
 
     std::cout << "After sort : " << arr[0] << " " << arr[1] << std::endl;
+  }
+
+  // 값과 상관 없이 항상 작은 값이 먼저 출력되게 하려면?
+  /** 주어진 배열이 정렬되어있는지 확인하는 로직 
+   * 00. 간단한 비교결과 출력
+   * 왼쪽 값이 오른쪽 값보다 작거나 같으면 true를 출력
+   * e.g) 
+   *    0 1 true
+   *    1 0 false
+   *    2 0 false
+   *    ...
+   *    0 1 true
+   *    1 1 true
+   *    2 1 false
+   *    3 1 false
+   *    4 1 false
+  */
+  {
+    std::cout<<std::endl;
+    std::cout << "### Compare sorted result ###"<< std::endl;
+    // 인자 한개만 넣어보는 경우 
+    compareTwoValue(0, 0);
+    // std::cout << std::endl;
+
+    // 첫 번째 인자를 배열로 주는 경우 
+    int arr[2] = {42,21};
+    for (size_t idx = 0; idx < (sizeof(arr) / sizeof(arr[0])); idx++)
+    {
+      compareTwoValue(arr[idx], 0);
+    }
+    std::cout << std::endl;
   }
 
   return 0;
