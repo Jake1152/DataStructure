333d354d682f9e0bdd6e29e76c17ac4033c227c6
Jake1152 <rancho974@gmail.com>
2024-02-27 03:09:01 +0900

    [WIP] Sort when array size three.
    - Does not work distincting mid, max, min value when are duplicated number exist

--- CPP/HongLab/ch01Basic/selectionSort/selectionSort.cpp
+++ CPP/HongLab/ch01Basic/selectionSort/selectionSort.cpp
@@ -9,6 +9,43 @@ void swap(int &a, int &b) {
 }
 
 /**
+ * ASC
+ * 특정 정렬방법을 쓰지 않고 
+ * 3개의 값만 들어온다는 점을 감안해서 만들어본다
+ * 값이 같은 경우가 있어서 min, max를 이용한 방식으로는 답이 안나올 수 있다.
+*/
+void MySort(int *arr, size_t size) 
+{
+  /**
+   *  1. a b c 
+   * 왼쪽이 오른쪽보다 작아야한다.
+  */
+  int max = arr[0];
+  int min = arr[0];
+  int mid = arr[0];
+  if (size == 3) {
+    for (int idx = 1; idx < size; idx++)
+    {
+      if (arr[idx] > max)
+        max = arr[idx];
+      if (arr[idx] < min)
+        min = arr[idx];
+    }
+    for (int idx = 1; idx < size; idx++)
+    {
+      if (arr[idx] >= min && arr[idx] <= max)
+        mid = arr[idx];
+    }
+    arr[0] = min;
+    arr[1] = mid;
+    arr[2] = max;
+  } else {
+    // other sort way
+    return ;
+  }
+}
+
+/**
  * CPP function naming convention은 Pascal
  * 
  * ref: https://www.geeksforgeeks.org/naming-convention-in-c/
@@ -40,6 +77,7 @@ int main() {
           std::cout << " -> " << std::flush;
 
           // TODO: 정렬 해보기
+          MySort(arr, 3);
 
           for (int e = 0; e < arr_size; e++)
           {
