119eeae485bf95146729d081aac15c5b8f0a9b3f
Jake1152 <rancho974@gmail.com>
2024-03-05 00:09:28 +0900

    [ADD] insertion sort
    - 정렬된 영역에 있는 값을 한 칸씩 오른쪽으로 밀어내기
    - 정렬된 영역에 집어 넣기 위해 뽑은 값이 현재 밀어내진 값보다 크거
      같다면 해당위치에 값을 넣는다.
    
    - e.g)
      8 3 2 5 1 2
        Before : 8 8 2 5 1 2
        After : 3 8 2 5 1 2
      3 8 2 5 1 2
        Before : 3 8 8 5 1 2
        After : 3 8 8 5 1 2
        Before : 3 3 8 5 1 2
        After : 2 3 8 5 1 2
      2 3 8 5 1 2
        Before : 2 3 8 8 1 2
        After : 2 3 8 8 1 2
        Before : 2 3 3 8 1 2
      2 3 5 8 1 2
        Before : 2 3 5 8 8 2
        After : 2 3 5 8 8 2
        Before : 2 3 5 5 8 2
        After : 2 3 5 5 8 2
        Before : 2 3 3 5 8 2
        After : 2 3 3 5 8 2
        Before : 2 2 3 5 8 2
        After : 1 2 3 5 8 2
      1 2 3 5 8 2
        Before : 1 2 3 5 8 8
        After : 1 2 3 5 8 8
        Before : 1 2 3 5 5 8
        After : 1 2 3 5 5 8
        Before : 1 2 3 3 5 8
        After : 1 2 3 3 5 8
        Before : 1 2 2 3 5 8
      1 2 2 3 5 8

---
+++ CPP/HongLab/ch01Basic/insertionSort/insertionSort.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+
+using namespace std;
+
+bool CheckSorted(int* arr, int size)
+{
+	for (int i = 0; i < size - 1; i++)
+	{
+		if (arr[i] > arr[i + 1])
+			return false;
+	}
+
+	return true;
+}
+
+void Print(int* arr, int size)
+{
+	for (int i = 0; i < size; i++)
+		cout << arr[i] << " ";
+	cout << endl;
+}
+
+void insertionSort(int *arr, size_t size)
+{
+	std::cout << "# " << std::flush;
+	Print(arr,size);
+	for (int i = 0; i < size; i++){
+		int current_picked_value = arr[i];
+		// 오른쪽으로 밀어내면서 복사
+		for (int j = i; j > 0; j--)
+		{
+			arr[j] = arr[j - 1];
+			std::cout << "    Before : " << std::flush;
+			Print(arr, size);
+			if (arr[j] <= current_picked_value)
+			{
+				arr[j] = current_picked_value;
+				break ;
+			} 
+			else if (j == 1)
+				arr[0] = current_picked_value;
+			std::cout << "    After : " << std::flush;
+			Print(arr, size);
+		}
+		std::cout << "  " << std::flush;
+		Print(arr, size);
+	}
+	std::cout << "# " << std::flush;
+	Print(arr,size);
+}
+
+int main()
+{
+	// 오른쪽으로 하나씩 밀기 (삽입 정렬의 중간 단계)
+	{
+		int arr[] = { 1, 2, 4, 5, 3, 6 };
+		int n = sizeof(arr) / sizeof(arr[0]);
+
+		Print(arr, n);
+
+		// i = 4인 경우에 대해서 구현
+		int i = 4;
+		int tmp = arr[i];
+		// 오른쪽으로 밀어내면서 복사
+		for (int idx = i; idx > 0; idx--)
+		{
+			arr[idx] = arr[idx - 1];
+			std::cout << "  " << std::flush;
+			Print(arr, n);
+		}
+
+		Print(arr, n);
+		cout << endl;
+
+		std::cout << std::boolalpha << "CheckSorted(arr, n) : " << CheckSorted(arr, n) << std::endl;
+	}
+
+	// int arr[] = { 8, 3, 2, 5, 1, 2 };
+	// int arr[] = { 6, 5, 4, 3, 2, 1 }; // Worst
+	int arr[] = { 1, 2, 3, 4, 5, 6 }; // Best
+	int n = sizeof(arr) / sizeof(arr[0]);
+
+	// Insertion Sort
+	{
+		insertionSort(arr, static_cast<size_t>(n));
+	}
+
+	// Stability
+}
