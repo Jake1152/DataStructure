f11a1d819f3dfe239baf86545573046ed0c12733
Jake1152 <rancho974@gmail.com>
2024-04-03 15:34:42 +0900

    [REF] Reimplement MoveDiks()

--- CPP/HongLab/ch04Stack/Hanoi/TowerOfHanoi.cpp
+++ CPP/HongLab/ch04Stack/Hanoi/TowerOfHanoi.cpp
@@ -8,7 +8,7 @@ Stack<char> tower[3];
 
 void PrintTowers()
 {
-	cout << "Towers" << endl;
+	cout << "# Towers" << endl;
 	cout << "0: ";
 	tower[0].Print();
 	cout << "1: ";
@@ -22,24 +22,19 @@ void MoveDisk(int from, int to)
 {
 	if (tower[from].IsEmpty())
 	{
-		cout << "Tower " << from << " is empty." << endl;
-		exit(0); // 오류 강제 종료
+		std::cout << "I can not move it" << std::endl;
+		std::cout << "Tower " << from << " is empty." << std::endl;
 	}
-
-	auto disk = tower[from].Top();
-
-	// 받을 타워가 비어 있으면 뭐든지 받을 수 있음
-	// 알파벳 순서여야 받을 수 있음 (역순 X)
+	const auto disk = tower[from].Top();
 	if (!tower[to].IsEmpty() && tower[to].Top() < disk)
 	{
-		cout << "Cannot place " << disk << " on " << tower[to].Top() << endl;
-		exit(0); // 오류 강제 종료
+		std::cout << "Cannot place " << disk << " on " << tower[to].Top() << std::endl;
+		exit(0);
 	}
-
 	tower[from].Pop();
 	tower[to].Push(disk);
 
-	cout << "Move disk " << disk << " from " << from << " to " << to << endl;
+	std::cout << "Move disk " << disk << " from " << from << " to " << to << std::endl;
 	PrintTowers();
 }
 
@@ -61,7 +56,7 @@ void RecurMoveDisks(int n, int from, int temp, int to)
 
 int main()
 {
-	int num_disks = 5;
+	int num_disks = 12;
 
 	for (int i = num_disks - 1; i >= 0; i--)
 		tower[0].Push('A' + i);
@@ -71,7 +66,7 @@ int main()
 	// MoveDisk(0, 2); // <- 디스크 하나만 움직이는 함수 tower 0 -> tower 2
 
 	RecurMoveDisks(num_disks, 0, 1, 2);
-
+	std::cout << "\n\n#Done"<< std::endl;
 	PrintTowers();
 
 	return 0;
