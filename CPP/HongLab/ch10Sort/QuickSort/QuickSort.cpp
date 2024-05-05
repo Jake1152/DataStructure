1dad62e07793f05709bfc542fe192d57542c4605
Jake1152 <rancho974@gmail.com>
2024-05-05 15:51:11 +0900

    [ADD] Quick sort

---
+++ CPP/HongLab/ch10Sort/QuickSort/QuickSort.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <iomanip>
+#include <math.h>
+
+using namespace std;
+
+
+template <typename T>
+void my_swap(T& a, T& b)
+{
+	T tmp;
+
+	tmp = a;
+	a = b;
+	b = tmp;
+}
+
+bool CheckSorted(int* arr, int size)
+{
+	for (int i = 0; i < size - 1; i++)
+	{
+		if (arr[i] > arr[i + 1])
+			return false;
+	}
+
+	return true;
+}
+
+void Print(int* arr, int low, int high, int n)
+{
+	for (int i = 0; i < n; i++)
+		if (i >= low && i <= high)
+			cout << setw(3) << arr[i] << " ";
+		else
+			cout << "    ";
+	cout << endl;
+}
+
+// Hoare partition scheme
+int Partition(int arr[], int low, int high, int n)
+{
+	int pivot = arr[size_t(floorf((high - low) / 2.0f)) + low];
+	// int i = low - 1;
+	// int j = high + 1;
+	int i = low;
+	int j = high;
+
+	// int i = low;
+	// int j = high;
+
+	while (true)
+	{
+		// TODO:
+		// for ()
+		for (;arr[i] < pivot;i++)
+			continue ;
+		for (;arr[j] > pivot;j--)
+			continue ;
+		my_swap(arr[i], arr[j]);
+		cout << "pivot=" << pivot << ", i=" << i << ", j=" << j << endl;
+		cout << "         ";
+		Print(arr, low, high, n);
+		if (i==j)
+			return i;
+	}
+}
+
+void QuickSort(int arr[], int low, int high, int n) // 마지막 n은 출력용
+{
+	if (low >= 0 && high >= 0 && low < high)
+	{
+		// Partitioning Index
+		cout << "# low=" << low << ", high=" << high << endl;
+		int p = Partition(arr, low, high, n);
+
+		QuickSort(arr, low, p, n); // Note: the pivot is now included
+		QuickSort(arr, p + 1, high, n);
+	}
+}
+
+int main()
+{
+	int arr[] = { 4, 17, 2, 9, 7, 5, 8, 1, 14, 6 };
+	int n = sizeof(arr) / sizeof(arr[0]);
+
+	cout << "         ";
+	Print(arr, 0, n - 1, n);
+	cout << endl;
+
+	QuickSort(arr, 0, n - 1, n);
+
+	cout << "         ";
+	Print(arr, 0, n - 1, n);
+	cout << endl;
+}
