f9c331420dbafa129ec39addc89eff7cbecd6d3f
Jake1152 <rancho974@gmail.com>
2024-05-08 18:43:06 +0900

    [ADD] Radix sort

--- CPP/HongLab/ch10Sort/RadixSort/RadixSort.cpp
+++ CPP/HongLab/ch10Sort/RadixSort/RadixSort.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 #include "../shared/Queue.h"
 
 using namespace std;
@@ -23,6 +24,8 @@ void Print(int* arr, int size)
 
 int GetMax(int arr[], int n)
 {
+	assert(arr);
+
 	int mx = arr[0];
 	for (int i = 1; i < n; i++)
 		if (arr[i] > mx)
@@ -41,11 +44,42 @@ int main()
 
 	Queue<int> queues[base_size];
 
-	int m = GetMax(arr, n); // 가장 큰 자리수를 찾기 위해서
+	int max_val = GetMax(arr, n); // 가장 큰 자리수를 찾기 위해서
 
-	for (int exp = 1; m / exp > 0; exp *= base_size)
+	for (int exp = 1; max_val / exp > 0; exp *= base_size)
 	{
 		// TODO:
+		// for (int element: arr)
+		// Print(arr, n);
+		for (int idx = 0; idx < n; idx++)
+		{
+			int element = arr[idx];
+			// 170 -> 0
+			// 421 -> 1
+			// modular 연산 사용
+
+			int divisor = static_cast<int>(base_size * exp);
+			// std::cout << "### element : " <<  element << std::endl;
+			// std::cout << "# divisor : " << divisor << std::endl;
+			int queue_idx = static_cast<int>(element % divisor) / exp;
+			// std::cout << "# queue_idx : " << queue_idx << std::endl;
+			queues[queue_idx].Enqueue(element);
+
+		}
+
+		int idx = 0;
+		// queue_idx < base_size;  대신 queue_idx < n;으로 해서 생겼던 문제
+		for (int queue_idx = 0; queue_idx < base_size; queue_idx++)
+		{
+			while (!(queues[queue_idx].IsEmpty()))
+			{
+				int current_val = queues[queue_idx].Front();
+				// std::cout << "current_val : " << current_val << std::endl;
+				queues[queue_idx].Dequeue();
+				arr[idx] = current_val;
+				idx++;
+			}
+		}
 
 		Print(arr, n);
 	}
